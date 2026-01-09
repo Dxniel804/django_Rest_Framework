380a787b2fd0ca586522075813ecab737048f061
Dxniel804 <dani.guto911@gmail.com>
2026-01-09 12:09:31 -0300

    feat: Add new views to code work

--- escola/escola/urls.py
+++ escola/escola/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include # include → importa rotas de outro app
+from cursos.urls import router
 
 urlpatterns = [
     path('api/v1/', include('cursos.urls')),
+    path('api/v2/', include(router.urls)),
     path('admin/', admin.site.urls),
     # Isso adiciona rotas de login/logout do Django REST Framework.
     path('auth/', include('rest_framework.urls'))
